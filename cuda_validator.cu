1ea1dd8f883b9858322c8a1f28ea6ab54dbafb49
Tuck Williamson <tuckwilliamson+ghcofc@gmail.com>
2024-10-08 11:41:58 -0400

    Made validation data in normal GPU memory not 'managed' shared memory with CPU. Also added error checking.

--- cuda_validator.cu
+++ cuda_validator.cu
@@ -3,6 +3,15 @@
 #include <iostream>
 #include <iomanip>
 
+#define cuEChk(toCheck) errChk(toCheck, __LINE__)
+void errChk(cudaError_t status, size_t line){
+    if(status != cudaSuccess){
+        std::cerr << "There was a cuda error at line " << line << "." << std::endl;
+        std::cerr << "Error (" << status << "): " << cudaGetErrorName(status) << "::" << cudaGetErrorString(status) << std::endl;
+        throw 1;
+    }
+}
+
 // Kernel function to add the elements of two arrays
 __global__ void validation_gen(const unsigned int maxIdx, unsigned int* dataPtr)
 {
@@ -15,31 +24,37 @@ __global__ void validation_gen(const unsigned int maxIdx, unsigned int* dataPtr)
 int main(int argc, char *argv[])
 {
   unsigned int N = 256<<2;
+  const size_t memSize = N*sizeof(dataPtr[0]);
   unsigned int* dataPtr;
 
   // Allocate Unified Memory - accessible from CPU or GPU
-  cudaMallocManaged(&dataPtr, N*sizeof(dataPtr[0]));
+//   cudaMallocManaged(&dataPtr, N*sizeof(dataPtr[0]));
+  cuEChk(cudaMalloc(&dataPtr, memSize));
 
   // Run kernel on 1M elements on the GPU
   validation_gen<<<1, 256>>>(N, dataPtr);
 
   // Wait for GPU to finish before accessing on host
-  cudaDeviceSynchronize();
+  cuEChk(cudaDeviceSynchronize());
+
+  uint16* hostPtr = (uint16*)malloc(memSize);
+  cuEChk(cudaMemcpy((void*)hostPtr, dataPtr, memSize, cudaMemcpyDeviceToHost));
 
   std::cout << std::hex;
   for(uint i = 0; i < N; i++){
-    auto idx = i & 0xffff80;
-    auto stride = i & 0x7f;
-    stride = stride << 16-5;
+    auto idx = i % 256;
+    auto stride = i / 256;
 
-    std::cout << "(" << std::dec << i << "): " << std::hex << dataPtr[i] << " -> " << stride << idx << " ";
+    std::cout << "(" << std::dec << i << "): " << std::hex << hostPtr[i*2] << hostPtr[i*2+1] << " -> " << stride << idx << " ";
     if(i % 4 == 0){
         std::cout << std::endl;
     }
   }
 
+  std::cout << std::endl << std::endl;
+
   // Free memory
-  cudaFree(dataPtr);
+  cuEChk(cudaFree(dataPtr));
 
   return 0;
 }
