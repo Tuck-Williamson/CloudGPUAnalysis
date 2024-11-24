f3df7073904a1414b52c1df17d4ecc87dcb21ca8
Tuck Williamson <tuckwilliamson+ghcofc@gmail.com>
2024-11-24 12:44:17 -0500

    Cleanup and fix errors from converting validation exe.

--- cuda_dev_tester.cu
+++ cuda_dev_tester.cu
@@ -13,12 +13,12 @@ void errChk(cudaError_t status, size_t line){
     }
 }
 
-__device__ const char[] devMessage= "\t\tHello from GPU.\n";
+__device__ char* devMessage= "\t\tHello from GPU.\n";
 
 // Kernel function to add the elements of two arrays
 __global__ void dev_tester(int dummy)
 {
-  printf("Hello from thread: %d.\n", thredIdx.x);
+  printf("Hello from thread: %d.\n", threadIdx.x);
   printf("\tTest message: %s\n", devMessage);
   printf("\tMessage Location: %p\n", devMessage);
   printf("\tKernel Location: %p\n", &dev_tester);
@@ -33,38 +33,10 @@ __global__ void dev_tester(int dummy)
 
 int main(int argc, char *argv[])
 {
-//   unsigned int N = 256<<2;
-//   unsigned int* dataPtr;
-//   const size_t memSize = N*sizeof(dataPtr[0]);
-//
-//   // Allocate Unified Memory - accessible from CPU or GPU
-// //   cudaMallocManaged(&dataPtr, N*sizeof(dataPtr[0]));
-//   cuEChk(cudaMalloc(&dataPtr, memSize));
-
-  // Run kernel on 1M elements on the GPU
-  dev_tester<<<1, 1>>>(N, dataPtr);
+  dev_tester<<<1, 1>>>(42);
 
   // Wait for GPU to finish before accessing on host
   cuEChk(cudaDeviceSynchronize());
-
-//   unsigned short* hostPtr = (unsigned short*)malloc(memSize);
-//   cuEChk(cudaMemcpy((void*)hostPtr, dataPtr, memSize, cudaMemcpyDeviceToHost));
-//
-//   std::cout << std::hex;
-//   for(uint i = 0; i < N; i++){
-//     auto idx = i % 256;
-//     auto stride = i / 256;
-//
-//     std::cout << "(" << std::dec << i << "): " << std::hex << hostPtr[i*2] << hostPtr[i*2+1] << " -> " << stride << idx << " ";
-//     if(i % 4 == 0){
-//         std::cout << std::endl;
-//     }
-//   }
-//
-//   std::cout << std::endl << std::endl;
-//
-//   // Free memory
-//   cuEChk(cudaFree(dataPtr));
 
   return 0;
 }
